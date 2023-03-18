ee5ff4bc7d8c83aef6ed96623f356170da4285ce
zeeshan-shabbir <zeeshanshabbir186@gmail.com>
2023-03-18 15:05:00 +0500

    api of file convert from zeeeshan

---
+++ indexFIleUpload.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+const multer  = require('multer');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/convert', upload.single('html'), async (req, res) => {
+  try {
+    const htmlFile = req.file;
+    console.log(htmlFile,"htmlFile");
+    const html = fs.readFileSync(htmlFile.path, 'utf-8');
+
+    // Compile the HTML template using Handlebars
+    const template = handlebars.compile(html);
+    const compiledHtml = template({});
+
+    // Launch a headless browser using Puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Set the page content to the compiled HTML
+    await page.setContent(compiledHtml);
+
+    // Generate a PDF with the same dimensions as the browser viewport
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: '10mm',
+        bottom: '10mm',
+        left: '10mm',
+        right: '10mm',
+      },
+    });
+
+    // Save the PDF to disk
+    const pdfPath = path.join(__dirname, 'public', 'output.pdf');
+    fs.writeFileSync(pdfPath, pdf);
+
+    const url = `http://localhost:5000/output.pdf`;
+    res.json({ url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error generating PDF' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
---
+++ indexMain.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.json({ limit: '50mb' }));
+const navigationTimeout = 60 * 1000
+app.post('/convert', async (req, res) => {
+  try {
+    const html = req.body.html;
+    // console.log(req.body,"req.body");
+
+    // Compile the HTML template using Handlebars
+    const template = handlebars.compile(html,{ strict: true });
+    const compiledHtml = template({});
+
+    // Launch a headless browser using Puppeteer
+    const browser = await puppeteer.launch({ headless: false , devtools: true });
+    const page = await browser.newPage();
+
+    // Set the page content to the compiled HTML
+    await page.setContent(compiledHtml, { timeout: navigationTimeout })
+    // const setContentPromise = new Promise((resolve, reject) => {
+    //   page.on('error', reject);
+    //   page.on('pageerror', reject);
+    //   page.on('load', resolve);
+    //   page.setContent(compiledHtml, { timeout: navigationTimeout })
+    // });
+
+    // await Promise.race([setContentPromise, new Promise((resolve, reject) => {
+    //   setTimeout(() => reject(new Error('Navigation timeout')), navigationTimeout);
+    // })]);
+
+    // Generate a PDF with the same dimensions as the browser viewport
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: '10mm',
+        bottom: '10mm',
+        left: '10mm',
+        right: '10mm',
+      },
+    });
+    await page.close()
+
+    // Save the PDF to disk
+    const pdfPath = path.join(__dirname, 'public', 'output.pdf');
+    fs.writeFileSync(pdfPath, pdf);
+
+    const url = `http://localhost:5000/output.pdf`;
+    res.json({ url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error generating PDF' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
