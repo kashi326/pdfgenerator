f772d8b8a7e7299535c6bf32ba237505b666e9a7
zeeshan-shabbir <zeeshanshabbir186@gmail.com>
2023-03-20 14:39:02 +0500

    index .js file changed same as index main

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ const puppeteer = require('puppeteer');
 const handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
-const multer  = require('multer');
 
 const app = express();
-const upload = multer({ dest: 'uploads/' });
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-app.post('/convert', upload.single('html'), async (req, res) => {
+app.use(bodyParser.json({ limit: '50mb' }));
+const navigationTimeout = 60 * 1000
+app.post('/convert-html', async (req, res) => {
   try {
-    const htmlFile = req.file;
-    console.log(htmlFile,"htmlFile");
-    const html = fs.readFileSync(htmlFile.path, 'utf-8');
+    const html = req.body.html;
+    // console.log(html,"htmlhtmlhtml");
 
     // Compile the HTML template using Handlebars
-    const template = handlebars.compile(html);
+    const template = handlebars.compile(html,{ strict: true });
     const compiledHtml = template({});
 
     // Launch a headless browser using Puppeteer
-    const browser = await puppeteer.launch({headless:false});
+    const browser = await puppeteer.launch({ headless: true , devtools: true, });
     const page = await browser.newPage();
 
     // Set the page content to the compiled HTML
-    await page.setContent(compiledHtml);
+    await page.setContent(compiledHtml, { timeout: 0 })
+    // const setContentPromise = new Promise((resolve, reject) => {
+    //   page.on('error', reject);
+    //   page.on('pageerror', reject);
+    //   page.on('load', resolve);
+    //   page.setContent(compiledHtml, { timeout: navigationTimeout })
+    // });
+
+    // await Promise.race([setContentPromise, new Promise((resolve, reject) => {
+    //   setTimeout(() => reject(new Error('Navigation timeout')), navigationTimeout);
+    // })]);
 
     // Generate a PDF with the same dimensions as the browser viewport
     const pdf = await page.pdf({
@@ -40,9 +47,10 @@ app.post('/convert', upload.single('html'), async (req, res) => {
         right: '10mm',
       },
     });
+    await page.close()
 
     // Save the PDF to disk
-    const pdfPath = path.join(__dirname, 'public', 'output.pdf');
+    const pdfPath = path.join(__dirname, 'uploads', 'output.pdf');
     fs.writeFileSync(pdfPath, pdf);
 
     const url = `http://localhost:5000/output.pdf`;
@@ -52,6 +60,15 @@ app.post('/convert', upload.single('html'), async (req, res) => {
     res.status(500).json({ error: 'Error generating PDF' });
   }
 });
+
+app.get('/',async (req, res)=>{
+  try {
+    res.json( 'server is running ');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error generating PDF' });
+  }
+})
 
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
