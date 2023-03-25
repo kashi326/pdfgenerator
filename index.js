729147604fd66c0ca0639d0fe9e52899ed2b3992
zeeshan-shabbir <zeeshanshabbir186@gmail.com>
2023-03-25 16:00:15 +0500

    changing pdf format for checking in print width issue

--- index.js
+++ index.js
@@ -35,15 +35,15 @@ app.post('/convert-html', async (req, res) => {
 
 		// Generate a PDF with the same dimensions as the browser viewport
 		const pdf = await page.pdf({
-			// format: 'A4',
-			// margin: {
-			// 	top: '10mm',
-			// 	bottom: '10mm',
-			// 	left: '10mm',
-			// 	right: '10mm',
-			// },
+			format: 'A4',
+			margin: {
+				top: '5mm',
+				bottom: '5mm',
+				left: '5mm',
+				right: '5mm',
+			},
 			printBackground: true,
-			preferCSSPageSize:true,
+			// preferCSSPageSize:true,
 		});
 		await page.close()
 		await browser.close()
